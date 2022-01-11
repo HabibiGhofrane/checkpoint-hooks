121d1e7f16f553d872f7655f0fc2b87786a22d9b
unknown <habibighofrane0000@gmail.com>
2022-01-11 14:18:46 +0100

    first commit

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import {Card, Button} from 'react-bootstrap'
+import ReactPlayer from 'react-player'
 import {  useNavigate, useParams } from 'react-router-dom'
 
 
 const Description = ({movies}) => {
     const navigate = useNavigate()
     const {id} = useParams()
-    const movieDisc = movies && movies.filter((movie) => movie.id === id)
+    const movieDisc = movies && movies.filter((movie) => movie.id == id)
+    
    
     return (
         <div>
 <Card>
   <Card.Header>{movieDisc.name}</Card.Header>
   <Card.Body>
-    <Card.Title>Special title treatment</Card.Title>
+    <Card.Title> Special title treatment </Card.Title>
     <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
+      <ReactPlayer url={movieDisc.trailerURL} />
     </Card.Text>
-    <Button variant="primary" onClick={()=>navigate (-1)}>Go Back </Button>
+    <Button variant="primary" onClick={()=> navigate (-1)}>Go Back </Button>
   </Card.Body>
 </Card>
         </div>
